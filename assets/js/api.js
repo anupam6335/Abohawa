bccfa73c71d75abf6f1ba13c8cac1031420f154d
Anupam Debnath <65649115+anupam6335@users.noreply.github.com>
2023-04-28 16:15:50 +0530

    api key setup

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,28 @@
-/**
- * @license MIT
- * @fileoverview All api related stuff like api_key, api request etc.
- * @copyright codewithsadee 2023 All rights reserved
- * @author codewithsadee <mohammadsadee24@gmail.com>
- */
+'use strict';
+
+const api_key = 'bb6475727c28652a6f5408d893a32783';
+
+// fetch data from server
+export const fetchData = (URL, callback) => {
+    fetch(`${URL}&appid=${api_key}`).then(res => res.json())
+    .then(data => callback(data));
+}
+
+
+export const URL = {
+    currentWeather(lat, lon) {
+        return `https://api.openweathermap.org/data/2.5/weather?${lat}&${lon}&units=metric`
+    },
+    forecast(lat, lon) {
+        return `https://api.openweathermap.org/data/2.5/forecast?${lat}&${lon}&units=metric`
+    },
+    airPolution(lat, lon) {
+        return `https://api.openweathermap.org/data/2.5/air_pollution?${lat}&${lon}`
+    },
+    reverseGeo(lat, lon) {
+        return `http://api.openweathermap.org/geo/1.0/reverse?${lat}&${lon}&limit=5`
+    },
+    geo(query) {
+        return `http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5`
+    }
+}
